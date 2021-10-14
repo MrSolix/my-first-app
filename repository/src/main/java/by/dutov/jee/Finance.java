5069a9338b9f8ca996d9d9225e6483b3ed269fd4
Slavik Dutov <solixjkeee@gmail.com>
2021-10-14 16:27:00 +0300

    03-Repositories-created

---
+++ repository/src/main/java/by/dutov/jee/Finance.java
@@ -0,0 +1,30 @@
+package by.dutov.jee;
+
+import by.dutov.jee.people.Teacher;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Finance {
+    public static Map<Teacher, Map<Integer, Double>> salaryHistory;
+
+    static {
+        salaryHistory = new HashMap<>();
+    }
+
+    public static double averageSalary(int minRange, int maxRange, Teacher teacher) {
+        int rangeCount = maxRange - minRange + 1;
+        double avgSal = 0.0;
+        if (salaryHistory.containsKey(teacher)) {
+            Map<Integer, Double> integerDoubleMap = salaryHistory.get(teacher);
+            for (int i = 1; i <= rangeCount; i++) {
+                avgSal += integerDoubleMap.get(i);
+            }
+        } else {
+            return -1;
+        }
+        return avgSal/rangeCount;
+    }
+
+
+}
